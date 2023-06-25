4f185dc53cef4bfc5450e058bbfad30a2fe03574
Kirill Vinogradov <vikian050194@gmail.com>
2023-06-25 13:20:15 +0200

    fix info icon cursor style

--- CHANGELOG.md
+++ CHANGELOG.md
@@ -7,6 +7,10 @@ and this project adheres to [Semantic Versioning](https://semver.org/spec/v2.0.0
 
 ## [Unreleased]
 
+### Fixed
+
+- Cursor is pointer in case of info icon hovering
+
 ## [1.1.0] - 2023-06-25
 
 ### Added
--- src/options/description.js
+++ src/options/description.js
@@ -33,32 +33,19 @@ export const descriptions = [
         ]
     ),
     makeDescription(
-        OPTIONS.IS_AUTOCLOSE_ENABLED,
-        "Autoclose enabled",
-        [
-            "If enabled then popup will be closed automatically after usage.",
-            "It's possible to cancel closing by any key pressing."
-        ]
-    ),
-    makeDescription(
-        OPTIONS.AUTOCLOSE_TIME,
-        "Autoclose time",
-        [
-            "Popup autoclose time in seconds."
-        ]
-    ),
-    makeDescription(
         OPTIONS.IS_AUTOMATIC_EXECUTION_ENABLED,
         "Automatic execution enabled",
         [
-            "Automatic execution start on extension activation."
+            "Automatic execution on extension activation.",
+            "If there is only one matched automatic target, then it will be executed."
         ]
     ),
     makeDescription(
         OPTIONS.IS_EXECUTION_LOGGING_ENABLED,
         "Execution logging enabled",
         [
-            "Log execution to active page Console."
+            "Log extension actions to active page Console.",
+            "It's useful for understanding of hard cases and unexpected behaviour."
         ]
     )
 ];
